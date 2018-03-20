38a6acfd4c78947d7c546e40ba215ef45132f150
Haoji Liu <haojiliu@Haojis-MacBook-Pro.local>
2018-03-19 19:33:19 -0700

    for testing...

--- project1/database_node/src/constants.py
+++ project1/database_node/src/constants.py
@@ -1,5 +1,5 @@
 zmq_read_host = 'cmpe275_task_scheduler'
-zmq_read_host = '172.18.0.3'
+zmq_read_host = '169.254.166.219'
 zmq_write_host = 'cmpe275_web'
 zmq_write_host = '172.18.0.4'
 read_worker_port = 5560
